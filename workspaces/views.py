69faca52eb39c953cb07a688dd7c2985d2dbf53f
mr-time2028 <hamidm.ap00@gmail.com>
2022-11-04 16:49:53 +0330

    Add WorkspaceView view

--- workspaces/views.py
+++ workspaces/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Workspace
+from .serializers import WorkspaceSerializer
+
+
+class WorkspaceView(viewsets.ViewSet):
+
+    @action(detail=False, methods=['get'])
+    def all_workspaces(self, request):
+        workspaces = Workspace.objects.filter(members=self.request.user)
+        serializer = WorkspaceSerializer(instance=workspaces, context={'request': request}, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
