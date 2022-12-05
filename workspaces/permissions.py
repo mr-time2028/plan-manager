66325d2838adb711bd01051067060a523919b3f8
mr-time2028 <hamidm.ap00@gmail.com>
2022-12-05 15:01:17 +0330

    Add permission to handle member and owner permissions

---
+++ workspaces/permissions.py
@@ -0,0 +1,31 @@
+from rest_framework import permissions
+
+from .models import WorkspaceUser
+
+
+class IsPartOfWorkspace(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user in obj.members.all()
+
+
+class IsWorkspaceOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        try:
+            workspace_user = WorkspaceUser.objects.get(workspace=obj, member=request.user)
+            return workspace_user.role == 'o'
+        except Exception:
+            return False
+
+
+class IsWorkspaceMember(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        try:
+            workspace_user = WorkspaceUser.objects.get(workspace=obj, member=request.user)
+            return workspace_user.role == 'm'
+        except:
+            return False
+
+
+class IsSuperuser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_superuser
