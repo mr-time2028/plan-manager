80f502671dff7b7c502b89b2b83ccf224683c3c7
mr-time2028 <hamidm.ap00@gmail.com>
2022-12-05 15:11:43 +0330

    Add change_owner signal to set new owner when owner left workspace

--- workspaces/signals.py
+++ workspaces/signals.py
@@ -1,8 +1,14 @@
 from django.utils.text import slugify
 from django.dispatch import receiver
-from django.db.models.signals import pre_save
+from django.db.models.signals import (
+    pre_save,
+    post_delete,
+)
 
-from .models import Workspace
+from .models import (
+    Workspace,
+    WorkspaceUser,
+)
 
 
 def unique_slug_gen(instance, field, new_slug=None):
@@ -23,3 +29,12 @@ def unique_slug_gen(instance, field, new_slug=None):
 def slug_generator(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = unique_slug_gen(instance, "title")
+
+
+@receiver(post_delete, sender=WorkspaceUser)
+def change_owner(sender, instance, *args, **kwargs):
+    if instance.role == 'o' and not WorkspaceUser.objects.filter(role='o').exists():
+        oldest_member = WorkspaceUser.objects.order_by("joined_at").first()
+        if oldest_member:
+            oldest_member.role = 'o'
+            oldest_member.save()
