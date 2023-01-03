ef29d511c1c2086318e96cd5a80382dd77644f2c
mr-time2028 <hamidm.ap00@gmail.com>
2023-01-03 18:19:37 +0330

    Add related_name and UniqueConstraint for models

--- workspaces/models.py
+++ workspaces/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 
 class Workspace(models.Model):
-    members = models.ManyToManyField(get_user_model(), through='WorkspaceUser')
+    members = models.ManyToManyField(get_user_model(), through='WorkspaceUser', related_name="workspace")
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=1024, allow_unicode=True, unique=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -20,8 +20,8 @@ class WorkspaceUser(models.Model):
         (OWNER, 'owner'),
         (MEMBER, 'member'),
     )
-    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
-    member = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE, related_name="workspace_user")
+    member = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="workspace_user")
     role = models.CharField(max_length=1, choices=ROLE_CHOICES, default='m')
     joined_at = models.DateTimeField(auto_now_add=True)
 
@@ -34,22 +34,27 @@ class WorkspaceUser(models.Model):
 class Board(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=1024, allow_unicode=True, unique=True, blank=True)
-    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
-    members = models.ManyToManyField(get_user_model(), through='BoardUser')
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE, related_name="board")
+    members = models.ManyToManyField(get_user_model(), through='BoardUser', related_name="board")
 
     def __str__(self):
         return self.title
 
 
 class BoardUser(models.Model):
-    board = models.ForeignKey(Board, on_delete=models.CASCADE)
-    member = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="board_user")
+    member = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="board_user")
     joined_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['board', 'member'], name='unique board member')
+        ]
 
 
 class Group(models.Model):
     title = models.CharField(max_length=255)
-    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="group")
 
     def __str__(self):
         return self.title
@@ -68,8 +73,8 @@ class Task(models.Model):
     )
 
     title = models.CharField(max_length=255)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    users = models.ManyToManyField(get_user_model())
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="task")
+    users = models.ManyToManyField(get_user_model(), related_name="task")
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     expire_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
