87e05b0ad1ce85e56856582edce135fd92b89603
mr-time2028 <hamidm.ap00@gmail.com>
2023-01-03 23:23:58 +0330

    Add urls for group section

--- workspaces/urls.py
+++ workspaces/urls.py
@@ -5,11 +5,13 @@ from rest_framework.routers import SimpleRouter
 from .views import (
     workspace_view,
     board_view,
+    group_view,
 )
 
 router = SimpleRouter()
 router.register(r'workspaces', workspace_view.WorkspaceView, basename='workspaces')
 router.register(r'boards', board_view.BoardView, basename='boards')
+router.register(r'groups', group_view.GroupView, basename='groups')
 
 
 app_name = "workspaces"
