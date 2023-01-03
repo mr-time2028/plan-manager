31972ce13365a3c2fb0b980b50fce0717909d452
mr-time2028 <hamidm.ap00@gmail.com>
2023-01-03 18:47:28 +0330

    Add board CRUD operations

--- workspaces/serializers/board_serializers.py
+++ workspaces/serializers/board_serializers.py
@@ -1,14 +1,54 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 
 from users.serializers import UserSerialzier
 from .group_serializers import GroupSerializer
-from ..models import Board
+from ..models import (
+    Board,
+    Task,
+    Workspace,
+)
 
 
 class BoardSerializer(serializers.ModelSerializer):
-    group_set = GroupSerializer(many=True, required=False, read_only=True)
+    group = GroupSerializer(many=True, required=False, read_only=True)
     members = UserSerialzier(many=True, required=False, read_only=True)
 
     class Meta:
         model = Board
-        fields = ("title", "slug", "group_set", "members")
+        fields = ("title", "slug", "group", "members")
+
+
+class BoardCreateSerializer(serializers.ModelSerializer):
+    group = serializers.SlugRelatedField(
+        many=True,
+        required=False,
+        slug_field="title",
+        queryset=Task.objects.all()
+    )
+    members = serializers.SlugRelatedField(
+        many=True,
+        required=False,
+        slug_field="username",
+        queryset=get_user_model().objects.all()
+    )
+    workspace = serializers.SlugRelatedField(
+        slug_field="slug",
+        queryset=Workspace.objects.all(),
+    )
+
+    class Meta:
+        model = Workspace
+        fields = ("title", "workspace", "slug", "group", "members")
+
+
+class BoardUpdateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Board
+        fields = ("title", "slug")
+
+
+class BoardUserSerialzier(serializers.Serializer):
+    members = serializers.ListField()
