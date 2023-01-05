27140ec60bf6c39b6586ca6edfc3043bbd5db6d8
mr-time2028 <hamidm.ap00@gmail.com>
2023-01-05 16:12:53 +0330

    Add url for task section

--- workspaces/urls.py
+++ workspaces/urls.py
@@ -3,15 +3,17 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
 from .views import (
-    workspace_view,
-    board_view,
-    group_view,
+    board_views,
+    group_views,
+    workspace_views,
+    task_views,
 )
 
 router = SimpleRouter()
-router.register(r'workspaces', workspace_view.WorkspaceView, basename='workspaces')
-router.register(r'boards', board_view.BoardView, basename='boards')
-router.register(r'groups', group_view.GroupView, basename='groups')
+router.register(r'workspaces', workspace_views.WorkspaceView, basename='workspaces')
+router.register(r'boards', board_views.BoardView, basename='boards')
+router.register(r'groups', group_views.GroupView, basename='groups')
+router.register(r'tasks', task_views.TaskView, basename='tasks')
 
 
 app_name = "workspaces"
