7bb7d4f4e9402c45f46df581d60df4245dbd19d9
mr-time2028 <hamidm.ap00@gmail.com>
2024-01-21 21:13:33 +0330

    Add update task logic

--- workspaces/utils.py
+++ workspaces/utils.py
@@ -8,15 +8,21 @@ from django.db.models import (
 
 
 def custom_filter(request: HttpRequest, queryset: QuerySet, filter_fields: List):
+    # Check if any filter parameters (including empty values) are provided
+    if not any(request.query_params.get(field) is not None for field in filter_fields):
+        return queryset
+
     filter_params = {}
     for field in filter_fields:
         filter_params[field] = request.query_params.get(field, None)
 
-    if filter_params:
-        q_objects = Q()
-        for field in filter_fields:
-            value = filter_params.get(field)
-            if value is not None:
-                q_objects &= Q(**{field: value})
-        return queryset.filter(q_objects)
-    return queryset
+    q_objects = Q()
+    for field in filter_fields:
+        value = filter_params.get(field)
+        # Include records where the field is either not None or an empty string
+        if value is not None:
+            q_objects &= Q(**{field: value})
+        
+        if value == '':
+            continue
+    return queryset.filter(q_objects)
